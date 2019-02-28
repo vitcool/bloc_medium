bb47ee6032c889f950c3244c05a889500fc92050
Vitalii Kulyk <vitcool@ukr.net>
2019-02-28 09:41:09 +0200

    BC-1: unsed comments are removed

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -4,7 +4,6 @@ import 'ui/movie_list.dart';
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return MaterialApp(
         theme: ThemeData.dark(), home: Scaffold(body: MovieList()));
   }
