831df27ea1c6a0369507c2036df41f22bc0eb765
Vitalii Kulyk <vitcool@ukr.net>
2019-03-03 14:07:27 +0200

    BC-2: movies list is moved to stateful

--- lib/src/ui/movie_list.dart
+++ lib/src/ui/movie_list.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
+
 import '../models/item_model.dart';
 import '../blocs/movies_bloc.dart';
 
-class MovieList extends StatelessWidget {
+class MovieList extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MovieListState();
+  }
+}
+
+class MovieListState extends State<MovieList> {
+  @override
+  void initState() {
+    super.initState();
+    bloc.fetchAllMovies();
+  }
+
+  @override
+  void dispose() {
+    bloc.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    bloc.fetchAllMovies();
     return Scaffold(
       appBar: AppBar(
         title: Text('Popular Movies'),
@@ -31,8 +50,7 @@ class MovieList extends StatelessWidget {
             new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
         itemBuilder: (BuildContext context, int index) {
           return Image.network(
-            'https://image.tmdb.org/t/p/w185${snapshot.data
-                .results[index].poster_path}',
+            'https://image.tmdb.org/t/p/w185${snapshot.data.results[index].poster_path}',
             fit: BoxFit.cover,
           );
         });
